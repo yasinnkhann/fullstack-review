50b72eb2bd5cc09e5016776c43a244d2a2b377dc
Yasin Khan <yasinkhan500@gmail.com>
2021-12-17 11:01:04 -0800

    set up schema

--- database/index.js
+++ database/index.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher2');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  _id: Number, // id
+  repoName: String, // name
+  username: String, // owner.login
+  userId: Number, // owner.id
+  githubUrl: String, // owner.url
+  reposUrl: String, // owner.repos_url
+  repoUrl: String, // html_url
+  forkCount: Number, // forks_count
+  starCount: Number, // stargazers_count
+  watcherCount: Number // watchers_count
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
